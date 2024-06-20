81a6d52922facb288eda76ac213cada97ea2bf53
KaitoDeCode <kurniawanadi4556@gmail.com>
2024-06-20 16:22:15 +0700

    boilerplate 1.0 finish

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -12,8 +12,7 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
-
+        $response = $this->getJson('/api/example');
         $response->assertStatus(200);
     }
 }
